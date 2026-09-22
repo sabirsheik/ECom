52cea0dbe1eb5d32d81c8eb51a629f18d8071084
Sabir Ali <sabirsheik12787@gmail.com>
2026-09-22 16:20:02 +0500

    rename product stock schema field

--- backend/models/products.js
+++ backend/models/products.js
@@ -16,7 +16,7 @@ const productScheme = new mongoose.Schema({
     discountPrice: {
         type: Number,
     },
-    countInPrice: {
+    countInStock: {
         type: Number,
         required: true,
         default: 0,
